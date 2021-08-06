eec5d57c9b65fd9fb8ebf0c2b15b599068155528
Trần Dung <88524911+Dungtran8@users.noreply.github.com>
2021-08-06 15:32:00 +0700

    Update App.java

--- src/main/java/com/onemount/App.java
+++ src/main/java/com/onemount/App.java
@@ -7,12 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int a = 2;
-    	int b = 3;
-    	if(a<b) {
-        System.out.println(a+2);
-    	}
-    	else System.out.println(" fail"+a);
-    System.out.println();
+        System.out.println("Hello world!");
     }
 }
